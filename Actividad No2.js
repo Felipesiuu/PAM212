bd03cf72bdf92c508f9148bfe0e8802cba6be21c
Felipesiuu <124049900@upq.edu.mx>
2025-09-05 23:42:05 -0600

    Se agregaron los metodos ala clase

--- Actividad No2.js
+++ Actividad No2.js
@@ -1,4 +1,19 @@
 class inicioPAM 
 {
+    DesplegarReglamentoPAM() {
+        console.log("Reglamento");
+    } 
 
+    DesplegarLineamientosClassroom() {
+        console.log("Lineamientos de Classroom");
+    }
+
+    DesplegarFechasdeParciales() {
+        console.log("Fechas de Parciales");
+    }
+
+    DesplegarPorcentajesporParcial() {
+        console.log("Porcentajes del parcial");
+    }
 }
+
