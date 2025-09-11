cc87b7504e7578f85c8b802cd149ba6c026b1a62
Felipesiuu <124049900@upq.edu.mx>
2025-09-11 10:56:49 -0600

    Variables
    Una pequeña descripción de como se usan  y como comentar dentro del código JS

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,1 +1,12 @@
-document.write("<h1>Hello World</h1>");
+// Como usar variables
+var name = "john";
+let lastname = "carter";
+
+// Se modifica la variable name
+name  = "pepe";
+
+// Las constantes no se pueden modificar
+const PI = 3.1415;
+
+console.log(PI);
+
