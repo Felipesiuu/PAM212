acd6c78522ef65e11383747ffe820493038a7730
Felipesiuu <124049900@upq.edu.mx>
2025-09-11 11:46:25 -0600

    Operadores y validaciones
    Se hacen operaciones de variables como operadores y concatenación , también se pueden usar validaciones como if y else o incluso un switch.

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,12 +1,67 @@
-// Como usar variables
-var name = "john";
-let lastname = "carter";
+// Operaciones
+// Con numeros 
+let numberOne = 60 ;
+let numberTwo = 100;
 
-// Se modifica la variable name
-name  = "pepe";
+let result = numberOne + numberTwo;
 
-// Las constantes no se pueden modificar
-const PI = 3.1415;
+console.log(result);
 
-console.log(PI);
+//string  
+let names = "Felipe";
+let lastname = "Munguia";
+// Concatenación
+let name = names + " " + lastname;
+console.log(name);
 
+
+// Comparación 
+// Puedes usar cualquier tipo de operadad 
+let result1 = numberOne > numberTwo;
+console.log(result1);
+
+
+let contra = "pepe";
+let input = "pepe1";
+
+let comprueba = input ==  contra;
+ 
+// Validar datos 
+if (comprueba == true)
+{
+    console.log("La contraseña es correcta");
+}
+else 
+{
+    console.log("Contraseña incorrecta");
+}
+
+let score = 10;
+if (score > 30)
+{
+    console.log("Eres bueno");
+}else if (score > 15)
+{
+    console.log("Tienes que mejorar");
+}
+else 
+{
+    console.log("Necesitas practicar");
+}
+
+
+// Manejo de switch
+
+let tarjeta = "Debito"
+
+switch(tarjeta)
+{
+    case "Debito":
+        console.log("Esta es una tarjeta de debito");
+        break;
+    case "Credito":
+        console.log ("Esta es una tarjeta de credito");
+        break;
+    default:
+        console.log ("No tienes tarjeta");
+}
