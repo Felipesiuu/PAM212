12070c394ceac1b43c3855c15ae99d828a5fb417
Felipesiuu <124049900@upq.edu.mx>
2025-09-18 19:11:40 -0600

    Ejercicio 2

--- Practica 1.1/app.js
+++ Practica 1.1/app.js
@@ -5,3 +5,10 @@ let edad = 18;
 let saludo = "Hola, " + nombre + ". Tienes " + edad + " años."
 
 console.log(saludo);
+
+//Ejercicio 2
+const cuadrado = numero => numero * numero;
+console.log(cuadrado(4));
+console.log(cuadrado(7));
+console.log(cuadrado(8));
+
