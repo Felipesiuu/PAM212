426af9480de333efc0a3bf34854a5e32cc7b05bf
Felipesiuu <124049900@upq.edu.mx>
2025-09-19 18:58:15 -0600

    Ejercicio 1 y ejercicio 2

---
+++ Practica2/Practica2.js
@@ -0,0 +1,28 @@
+//Ejercicio 1
+const persona = {
+    nombre: "Ivan Isay",
+    edad: 37,
+    direccion:{
+        ciudad:"Qro",
+        pais:"MX"
+    }
+};
+
+const{nombre, edad, direccion:{ciudad, pais}} = persona;
+
+console.log("Me llamo "+ nombre+", tengo " + edad + " años y vivo en " + ciudad +".");
+
+// Ejercicio 2
+const productos = [
+    { nombre: "Laptop", precio: 12000 },
+    { nombre: "Mouse", precio: 250},
+    { nombre: "Teclado", precio: 750 },
+    { nombre: "Monitor", precio: 3000 },
+];
+
+const nombres= productos
+.filter(producto => producto.precio > 1000)
+.map(producto => producto.nombre);
+console.log(nombres);
+
+
