83f9388929ab710ca38d00e6b5ff4b87b88cb102
Felipesiuu <124049900@upq.edu.mx>
2025-09-25 18:05:42 -0600

    Ejercicio a

---
+++ Practica3/main.js
@@ -0,0 +1,7 @@
+import { restar } from './utils.js';
+
+console.log(restar(2, 3));
+console.log(restar(10, 4));
+console.log(restar(82, 23));
+console.log(restar(13, 12));
+console.log(restar(24, 23));
