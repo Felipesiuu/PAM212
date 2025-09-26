e82369d50d58d54c01f47b219e7ecb7ad3cbf899
Felipesiuu <124049900@upq.edu.mx>
2025-09-25 18:24:56 -0600

    ejercicio b

--- Practica3/main.js
+++ Practica3/main.js
@@ -5,3 +5,23 @@ console.log(restar(10, 4));
 console.log(restar(82, 23));
 console.log(restar(13, 12));
 console.log(restar(24, 23));
+
+
+function verificarUsuario(usuario) {
+    return new Promise((resolve, reject) => {
+        if (usuario === "admin") 
+            {
+            resolve("Acceso concedido");
+        } else 
+        {
+             reject("Acceso denegado");
+        }
+  });
+}
+ verificarUsuario("admin")
+    .then(res => console.log(res))// Acesso concedido
+    .catch(err => console.error(err));
+
+verificarUsuario("Ivan")
+    .then(res => console.log(res))
+    .catch(err => console.error(err)); // Acesso denegado
--- Practica3/utils.js
+++ Practica3/utils.js
@@ -1,3 +1,5 @@
  export function restar(a, b){
     return a - b;
 }
+
+
